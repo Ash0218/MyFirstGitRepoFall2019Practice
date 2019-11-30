fe6170374468f5a74ac60e9f04a471c7dfbfc444
Ash0218 <ajykim218@gmail.com>
2019-11-30 13:17:30 -0500

    Second commit. Good evening. JavaClass1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
+public class HelloWorld { // one
     public static void main(String[] args) { // 1
         System.out.println("Hello World"); // 2
+        System.out.println("Good evening"); // 3
 
     }
 }
